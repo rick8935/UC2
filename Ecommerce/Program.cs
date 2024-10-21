0b5f101f938ec890692a6bc5ac7c7aae25dece2b
rick8935 <senac.rickboone@gmail.com>
2024-10-21 20:26:25 -0300

    Etapa 2

--- Ecommerce/Program.cs
+++ Ecommerce/Program.cs
@@ -1,24 +1,54 @@
 ﻿using System;
-using System.ComponentModel.Design;
+using System.Collections.Generic;
 
 namespace Ecommerce
 {
 
+    public class Categoria
+    {
+        public string Nome { get; private set; }
+
+        public Categoria(string nome)
+        {
+            Nome = nome;
+        }
+    }
+
+    
+    public class Fornecedor
+    {
+        public string Nome { get; private set; }
+
+        public Fornecedor(string nome)
+        {
+            Nome = nome;
+        }
+    }
+
+    
+    public class Produto
+    {
+        public string Nome { get; private set; }
+        public Categoria Categoria { get; private set; }
+        public Fornecedor Fornecedor { get; private set; }
+
+        public Produto(string nome, Categoria categoria, Fornecedor fornecedor)
+        {
+            Nome = nome;
+            Categoria = categoria;
+            Fornecedor = fornecedor;
+        }
+    }
+
     internal class Program
     {
-        const int LIMITE = 30;
-        static string[] categorias = new string[LIMITE];
-        static string[] fornecedores = new string[LIMITE];
-        static string[] produtos = new string[LIMITE];
-        static int[] produtoCategorias = new int[LIMITE];
-        static int[] produtoFornecedor = new int[LIMITE];
-        static int contadorCategoria = 0;
-        static int contadorFornecedor = 0;
-        static int contadorProduto = 0;
+        static List<Categoria> categorias = new List<Categoria>();
+        static List<Fornecedor> fornecedores = new List<Fornecedor>();
+        static List<Produto> produtos = new List<Produto>();
 
         static void Main(string[] args)
         {
-            int opcao = 0;
+            int opcao;
 
             do
             {
@@ -78,75 +108,45 @@ namespace Ecommerce
 
         static void modulo_categoria()
         {
-            if(contadorCategoria < 10)
-            {
-                Console.WriteLine("Digite o nome da nova categoria (ex: Smartphones, Acessórios, etc.): ");
-                categorias[contadorCategoria] = Console.ReadLine();
-                contadorCategoria++;
-                Console.WriteLine("Categoria cadastrada com sucesso!");
-            }
-            else
-            {
-                Console.WriteLine("Limite de categorias atingido");
-            }
+            Console.WriteLine("Digite o nome da nova categoria (ex: Smartphones, Acessórios, etc.): ");
+            string nome = Console.ReadLine();
+            categorias.Add(new Categoria(nome));
+            Console.WriteLine("Categoria cadastrada com sucesso!");
         }
 
         static void modulo_fornecedor()
         {
-            if(contadorFornecedor < 10)
-            {
-                Console.WriteLine("Digite o nome do fornecedor (ex: Samsung, Apple, Xiaomi): ");
-                fornecedores[contadorFornecedor] = Console.ReadLine();
-                contadorFornecedor++;
-                Console.WriteLine("Fornecedor cadastrado com sucesso!");
-            }
-            else
-            {
-                Console.WriteLine("Limite de fornecedores atingido!");
-            }
+            Console.WriteLine("Digite o nome do fornecedor (ex: Samsung, Apple, Xiaomi): ");
+            string nome = Console.ReadLine();
+            fornecedores.Add(new Fornecedor(nome));
+            Console.WriteLine("Fornecedor cadastrado com sucesso!");
         }
 
         static void modulo_produto()
         {
-            int categoria_escolhida = 0;
-            int fornecedor_escolhido = 0;
+            Console.WriteLine("Digite o nome do produto (ex: iPhone 13, Galaxy S21): ");
+            string nome = Console.ReadLine();
 
-            if(contadorProduto < 10)
+            Console.WriteLine("Escolha uma categoria para o produto: ");
+            listar_categorias();
+            int categoriaEscolhida = int.Parse(Console.ReadLine()) - 1;
 
-            {
-                Console.WriteLine("Digite o nome do produto (ex: iPhone 13, Galaxy S21): ");
-                produtos[contadorProduto] = Console.ReadLine();
+            Console.WriteLine("Escolha um fornecedor para o produto: ");
+            listar_fornecedores();
+            int fornecedorEscolhido = int.Parse(Console.ReadLine()) - 1;
 
-                Console.WriteLine("Escolha uma categoria para o produto: ");
-                listar_categorias();
-                Console.WriteLine("Digite o número da categoria: ");
-                categoria_escolhida = int.Parse(Console.ReadLine());
-                produtoCategorias[contadorProduto] = categoria_escolhida - 1;
-                Console.WriteLine("Escolha um fornecedor para o produto: ");
-                listar_fornecedores();
-                Console.WriteLine("Digite o número do fornecedor: ");
-                fornecedor_escolhido = int.Parse(Console.ReadLine());
-                produtoFornecedor[contadorProduto] = fornecedor_escolhido - 1;
-
-                contadorProduto++;
-                Console.WriteLine("Produto cadastrado com sucesso!");
-            }
-            else
-            {
-                Console.WriteLine("Limite de produtos atingido!");
-            }
+            produtos.Add(new Produto(nome, categorias[categoriaEscolhida], fornecedores[fornecedorEscolhido]));
+            Console.WriteLine("Produto cadastrado com sucesso!");
         }
 
         static void listar_categorias()
         {
-
-            if(contadorCategoria > 0)
+            if (categorias.Count > 0)
             {
-                    Console.WriteLine("CATEGORIAS CADASTRADAS: ");
-
-                for(int i = 0; i < contadorCategoria; i++)
-			    {
-                    Console.WriteLine (i + 1 + ". " + int.Parse(categorias[i]));
+                Console.WriteLine("CATEGORIAS CADASTRADAS: ");
+                for (int i = 0; i < categorias.Count; i++)
+                {
+                    Console.WriteLine(i + 1 + ". " + categorias[i].Nome);
                 }
             }
             else
@@ -157,13 +157,12 @@ namespace Ecommerce
 
         static void listar_fornecedores()
         {
-            if (contadorFornecedor > 0)
+            if (fornecedores.Count > 0)
             {
                 Console.WriteLine("FORNECEDORES CADASTRADOS: ");
-
-                for (int i = 0; i < contadorFornecedor; i++)
+                for (int i = 0; i < fornecedores.Count; i++)
                 {
-                    Console.WriteLine(i + 1 + ". " + fornecedores[i]);
+                    Console.WriteLine(i + 1 + ". " + fornecedores[i].Nome);
                 }
             }
             else
@@ -174,13 +173,12 @@ namespace Ecommerce
 
         static void listar_produtos()
         {
-            if(contadorProduto > 0)
+            if (produtos.Count > 0)
             {
                 Console.WriteLine("PRODUTOS CADASTRADOS: ");
-
-                for(int i = 0; i < contadorProduto; i++)
-			    {
-                    Console.WriteLine(i + 1 + ". Produto: " + produtos[i] + ", Categoria: " + categorias[produtoCategorias[i]] + ", Fornecedor: " + fornecedores[produtoFornecedor[i]]);
+                foreach (var produto in produtos)
+                {
+                    Console.WriteLine($"Produto: {produto.Nome}, Categoria: {produto.Categoria.Nome}, Fornecedor: {produto.Fornecedor.Nome}");
                 }
             }
             else
@@ -188,47 +186,5 @@ namespace Ecommerce
                 Console.WriteLine("Nenhum produto cadastrado.");
             }
         }
-
-        static void popular_banco_dados()
-        {
-            string[] categorias_lote = new string[5] { "Smartphones", "Acessórios", "Carregadores", "Capinhas", "Fones de ouvido" };
-            string[] fornecedores_lote = new string[5] { "Samsung", "Apple", "Xiaomi", "Motorola", "LG" };
-            string[] produtos_lote = new string[5] { "Galaxy S21", "iPhone 13", "Redmi Note 10", "Moto G100", "LG Velvet" };
-
-
-            // Cadastrar categorias em lote
-            for(int i = 0; i < 5; i++)
-		    {
-                if(contadorCategoria < 10)
-                {
-                    categorias[contadorCategoria] = categorias_lote[i];
-                    contadorCategoria++;
-                }
-            }
-
-            // Cadastrar fornecedores em lote
-            for(int i = 0; i < 5; i++)
-		    {
-                if(contadorFornecedor < 10)
-                {
-                    fornecedores[contadorFornecedor] = fornecedores_lote[i];
-                    contadorFornecedor++;
-                }
-            }
-
-            // Cadastrar produtos em lote com suas respectivas categorias e fornecedores
-            for(int i = 0; i < 5; i++)
-		    {
-                if(contadorProduto < 10)
-                {
-                    produtos[contadorProduto] = produtos_lote[i];
-                    produtoCategorias[contadorProduto] = i; // Cada produto recebe a categoria correspondente
-                    produtoFornecedor[contadorProduto] = i;  // Cada produto recebe o fornecedor correspondente
-                    contadorProduto++;
-                }
-            }
-
-            Console.WriteLine("Banco de dados inicializado com sucesso!");
-        }
     }
- }
+}
